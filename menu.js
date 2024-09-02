11dd92941a19214dfd38fc92bb86818b6a151818
matheusschramm <matheusscramm147@gmail.com>
2024-09-01 21:10:50 -0300

    Primeiro commit

---
+++ menu.js
@@ -0,0 +1,24 @@
+let menuItem  =  document.querySelectorAll('.item-menu');
+
+function selectLink(){
+    menuItem.forEach(item => 
+        item.classList.remove('ativo')
+    );
+    this.classList.add('ativo');
+}
+
+menuItem.forEach((item) => 
+    item.addEventListener('click', selectLink)
+);
+
+// expandindo pelo botao de expandie
+var btnExp = document.querySelector('#btnExp');
+var ladoMenu = document.querySelector('.menu-lateral');
+var logo = document.querySelector('.logo-hidden');
+
+btnExp.addEventListener('click', function(){
+    ladoMenu.classList.toggle('expand');
+    logo.classList.toggle('logo-divero');
+    logo.classList.toggle('logo-hidden');
+    
+})
