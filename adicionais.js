3cfd36812a07ef38c50a6baa42371f6caa41a235
matheusschramm <matheusscramm147@gmail.com>
2024-10-07 22:08:22 -0300

    ok

--- adicionais.js
+++ adicionais.js
@@ -1,3 +1,22 @@
-const subtitulos = document.querySelectorAll('.subtitulos');
 
-console.log(subtitulos);
+function formataSubtitulo(){
+    if(window.innerWidth > 970){
+        let subtitulos = document.querySelectorAll('.subtitulo');
+        
+        subtitulos.forEach( subtitulo => {
+            console.log(subtitulo.textContent);
+        
+            let texto = subtitulo.textContent;
+        
+            if(texto.includes('-')){
+        
+                let partesTexto = texto.split('-');
+        
+                subtitulo.innerHTML = `${partesTexto[0].trim()} - <strong style="margin-left: 7px; line-height: 1.35cap;">  ${partesTexto[1].trim()}</strong>`;
+            }
+        });
+    }
+};
+
+formataSubtitulo();
+window.addEventListener('resize', formataSubtitulo);
